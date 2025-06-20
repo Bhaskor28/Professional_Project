5ef06b0cc695e1d157a87b56ce94d9ea67b42ae4
Bhaskor Roy <112170792+Bhaskor28@users.noreply.github.com>
2025-06-20 20:41:13 +0600

    Add files via upload

---
+++ EmployeeManagementSystmem/EmployeeManagementSystem.Infrastructure/Data/AppDbContext.cs
@@ -0,0 +1,24 @@
+﻿using EmployeeManagementSystem.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmployeeManagementSystem.Infrastructure.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
+            
+        }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.ApplyConfigurationsFromAssembly(typeof(AppDbContext).Assembly);
+        }
+        public DbSet<Employee> Employees { get; set; }
+        public DbSet<Department> Departments { get; set; }
+    }
+}
---
+++ EmployeeManagementSystmem/EmployeeManagementSystem.Infrastructure/DependencyRegister.cs
@@ -0,0 +1,26 @@
+﻿using EmployeeManagementSystem.Application.Departments;
+using EmployeeManagementSystem.Application.Employees;
+using EmployeeManagementSystem.Application.Repository;
+using EmployeeManagementSystem.Infrastructure.Data;
+using EmployeeManagementSystem.Infrastructure.Repository;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace EmployeeManagementSystem.Infrastructure
+{
+    public static class DependencyRegister
+    {
+        public static IServiceCollection AddInfrastructureServices(this IServiceCollection services, IConfiguration configuration)
+        {
+            services.AddDbContext<AppDbContext>(options =>
+            {
+                options.UseSqlServer(configuration.GetConnectionString("Connection"));
+            });
+            services.AddScoped<IEmployeeService, EmployeeService>();
+            services.AddScoped<IDepartmentService, DepartmentService>();
+            services.AddScoped(typeof(IBaseRepository<>), typeof(BaseRepository<>));
+            return services;
+        }
+    }
+}
