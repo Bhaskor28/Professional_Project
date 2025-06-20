1d9ef152d1aff6222b18f97def6bfd016ee9fb6e
Bhaskor Roy <112170792+Bhaskor28@users.noreply.github.com>
2025-06-20 20:31:15 +0600

    Add files via upload

---
+++ EmployeeManagementSystmem/EmployeeManagementSystem.Application/Departments/IDepartmentService.cs
@@ -0,0 +1,11 @@
+﻿using EmployeeManagementSystem.Application.Employees;
+
+namespace EmployeeManagementSystem.Application.Departments
+{
+    public interface IDepartmentService
+    {
+        Task AddNewDepartmentAsync(DepartmentVM department);
+        Task<IEnumerable<DepartmentVM>> GetAllDepartment();
+
+    }
+}
---
+++ EmployeeManagementSystmem/EmployeeManagementSystem.Application/Employees/IEmployeeService.cs
@@ -0,0 +1,12 @@
+﻿using EmployeeManagementSystem.Application.Common.PaginatedList;
+
+namespace EmployeeManagementSystem.Application.Employees
+{
+    public interface IEmployeeService
+    {
+        Task AddNewEmployeeAsync(EmployeeVM employeeVM);
+        Task<IEnumerable<EmployeeVM>> GetAllEmployees();
+        Task<PaginatedList<EmployeeVM>> GetPagedEmployeesAsync(string? search, string? sortOrder, int pageNumber, int pageSize);
+
+    }
+}
